afe6326a3d6d0e1449d28e676d41ed803f1a20fe
Sajeel Zafar <sajeelzafar1995@gmail.com>
2022-12-17 09:59:22 +0500

    Updated the files with sql codes

--- data.sql
+++ data.sql
@@ -16,3 +16,23 @@ VALUES (5, 'Charmander', '02-08-2020', 0, 'False', -11),
 (9, 'Boarmon', '06-07-2005', 7, 'True', 20.4),
 (10, 'Blossom', '10-13-1998', 3, 'True', 17),
 (11, 'Ditto', '05-14-2022', 4, 'True', 22);
+
+INSERT INTO owners (full_name, age)
+VALUES ('Sam Smith', 34),
+('Jennifer Orwell', 19),
+('Bob', 45),
+('Melody Pond', 77),
+('Dean Winchester', 14),
+('Jodie Whittaker', 38);
+
+INSERT INTO species (name)
+VALUES ('Pokemon'),
+('Digimon');
+
+UPDATE animals SET species_id = 1 WHERE name LIKE '%mon';
+UPDATE animals SET species_id = 2 WHERE name NOT LIKE '%mon';
+UPDATE animals SET owner_id = 1 WHERE name IN ('Agumon');
+UPDATE animals SET owner_id = 2 WHERE name IN ('Gabumon','Pikachu');
+UPDATE animals SET owner_id = 3 WHERE name IN ('Devimon','Plantmon');
+UPDATE animals SET owner_id = 4 WHERE name IN ('Charmander','Squirtle','Blossom');
+UPDATE animals SET owner_id = 5 WHERE name IN ('Angemon','Boarmon');
